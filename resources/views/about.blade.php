c53938c68e5910ae17ed82e46ee5561f8936d7d8
joadisorn <joadisorn331@gmail.com>
2021-02-13 14:28:33 +0700

    crevle

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">Laravel</a> | <a href="/about">about</a> | <a href="contact">contact</a>
-    <h1>สวัสดี about</h1>
-</body>
-</html>
+@extends('layout.master')
+
+@section('content')
+<h1>สวัสดี about</h1>
+
+@endsection
+
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Laravel</a> | <a href="/about">about</a> | <a href="contact">contact</a>
+    @yield("content")
+    <hr>
+    &copy; 2021 Adisorn Nampia
+</body>
+</html>
